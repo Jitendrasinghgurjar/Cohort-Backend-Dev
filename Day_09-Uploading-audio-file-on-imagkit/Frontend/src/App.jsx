60933bcef6a25d796e06902d397f2a277f35bc8b
jitendrasinghgurjar <23jitugurjar@gmail.com>
2025-08-19 23:24:26 +0530

    Day 9: Uploaded audio file to ImageKit

---
+++ Day_09-Uploading-audio-file-on-imagkit/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import FacialExpression from './Components/facialExpression'
+import './App.css'
+import MoodSongs from './Components/MoodSongs'
+
+function App() {
+
+
+  return (
+    <>
+      <FacialExpression />
+      <MoodSongs />
+    </>
+  )
+}
+
+export default App
