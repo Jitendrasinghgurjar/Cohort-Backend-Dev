86a5e52b1ce3989c4dbe71884cffc1a95a9a383a
jitendrasinghgurjar <23jitugurjar@gmail.com>
2025-08-26 19:24:56 +0530

    Day 12–13: Learned Authentication

---
+++ Day_12-13-Authentication/src/routes/auth.routes.js
@@ -0,0 +1,63 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+
+const router = express.Router()
+
+
+
+/* 
+
+POST /register
+POST /login
+GET /user
+GET /logout
+
+
+*/
+
+
+router.post('/register',async (req,res)=>{
+
+    const {username,password} = req.body 
+    
+
+    const user = await userModel.create({
+        username,password
+    })
+
+    
+    res.status(201).json({
+        message:"user registered successfully",
+        user
+    })
+    
+})
+
+router.post('/login',async(req,res)=>{
+    const {username,password} = req.body
+
+    const user = await userModel.findOne({
+        username:username
+    })
+
+    if(!user){
+       return res.status(401).json({
+            message:"user account not found [ invalid username ]"
+        })
+    }
+
+    const isPasswordValid = password == user.password
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message :"Invalid password"
+        })
+    }
+
+    res.status(200).json({
+        message:"user loggedIn successfully"
+    })
+})
+
+
+module.exports = router
