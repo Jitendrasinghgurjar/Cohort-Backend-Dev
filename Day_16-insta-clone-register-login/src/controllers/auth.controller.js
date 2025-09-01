5628bcfa74a545561d1ad78730bb47d591aa1928
jitendrasinghgurjar <23jitugurjar@gmail.com>
2025-09-01 02:07:59 +0100

    Day 16: Mini Insta Clone – Added JWT authentication & password hashing

---
+++ Day_16-insta-clone-register-login/src/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+const userModel = require("../models/user.model")
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+async function registerController(req,res){
+    const{username,password} = req.body
+    const isuserAlreadyExists = await userModel.findOne({
+        username
+    })
+    if(isuserAlreadyExists){
+        return res.status(400).json({
+            message: "user already exists"
+        })
+        }
+        const user = await userModel.create({
+            username,
+            password: await bcrypt.hash(password,10)
+        
+        })
+    const token = jwt.sign({id:user._id},
+    process.env.JWT_SECRET)
+
+res.cookie = ("token",token)
+return res.status(200).json({
+    message: "user created successfully",
+    user
+})
+}
+
+async function loginController(req,res){
+    const {username,password} = req.body
+    const user = await userModel.findOne({
+        username
+    })
+    if(!user){
+        return res.status(400).json({
+            message: "user account not found"
+        })
+    }
+    const ispasswordvalid = await bcrypt.compare(password, user.password)
+    if(!ispasswordvalid){
+        return res.status(400).json({
+            message: "invalid password"
+        })
+}
+const token = jwt.sign({
+    id:user._id},
+    process.env.JWT_SECRET
+)
+res.cookie("token",token)
+return res.status(200).json({
+    message: "login sucessfully",
+    token
+})
+}
+module.exports= {
+    registerController,
+    loginController
+}
