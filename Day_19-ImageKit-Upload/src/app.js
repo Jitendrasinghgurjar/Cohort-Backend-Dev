ecff0672653614cdd86712bb6308991cb952c882
jitendrasinghgurjar <23jitugurjar@gmail.com>
2025-09-10 05:12:47 +0100

    Day 19: ImageKit upload feature

---
+++ Day_19-ImageKit-Upload/src/app.js
@@ -0,0 +1,12 @@
+    const express = require('express')
+    const authroutes = require('./routes/auth.routes')
+    const postroutes = require('./routes/post.routes')
+    const cookieParser = require('cookie-parser')
+    const app = express()
+    app.use(express.json())
+    app.use(cookieParser())
+    app.use('/api/auth',authroutes)
+    app.use('/api/posts',postroutes)
+
+
+    module.exports = app
